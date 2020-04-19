3c87d369d095037e382af8f7b36aca16a7bc56d2
sonysebastian <sonysebastian@qburst.com>
2020-04-19 23:58:36 +0530

    add node type declaration

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,11 +1,11 @@
 // TypeScript Version: 3.0
 
-  export interface Variant {
-    hashRaw: (...args: any[]) => Promise<any>;
-    hash: (...args: any[]) => Promise<any>;
-    verify: (encoded: any, password: any) => Promise<any>;
-  }
-  export function variant(hashRaw: any, hashEncoded: any, verify: any): Variant;
+export interface Variant {
+  hashRaw: (...args: any[]) => Promise<any>;
+  hash: (...args: any[]) => Promise<any>;
+  verify: (encoded: any, password: any) => Promise<any>;
+}
+export function variant(hashRaw: any, hashEncoded: any, verify: any): Variant;
 
-  export const argon2i: Variant;
-  export const argon2d: Variant;
+export const argon2i: Variant;
+export const argon2d: Variant;
